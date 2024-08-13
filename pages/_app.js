800d20c8fe1e348726f3891f10f1a449db12cd48
Aravindh2501 <muthuaravindh2512001@gmail.com>
2024-08-13 19:39:39 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
+import Transition from "@/components/Transition";
+
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
+};
+
+export default MyApp;
